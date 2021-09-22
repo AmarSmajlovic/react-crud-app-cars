9120d4ef51a51e76095090406e034e2d3a1fbb41
AmarSmajlovic <amarcsgo@gmail.com>
2021-09-22 19:36:45 +0200

    edit function add in input values

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -5,6 +5,15 @@ import Modal from "react-bootstrap/Modal";
 function Table({ products, onClick,deleteCar }) {  
 
 
+  const [mark,setMark] = useState([]);
+  useEffect(()=>{
+      axios.get('http://localhost:8000/marksName')
+      .then(res=>setMark(res.data))
+      .catch(err=>console.log(err));
+  },[])
+  
+
+
 
   const handleDeleteCar = (e) => {
      axios.delete(`http://localhost:8000/deleteModel/${e.target.id}`)
@@ -15,22 +24,36 @@ function Table({ products, onClick,deleteCar }) {
       .catch(err=>console.log(err));  
   }
 
+  const [show,setShow] = useState(false);
+  const [name,setName] = useState("");
+  const [image,setImage] = useState("");
+  const [price,setPrice] = useState("");
+  const [model,setModel] = useState("");
   
   
 
-  const editCar = () => {
-  // function for edit car need imput
+  const handleEditCar = (e) => {
+    axios.get(`http://localhost:8000/getCar/${e.target.id}`)
+    .then(res=>{
+          setName(res.data[0].name)
+          setImage(res.data[0].image)
+          setPrice(res.data[0].price)
+          setModel(res.data[0].modelId)
+    })
+    setShow(true);
   };
 
+  const submitEditCar = () => {
+   console.log('susscesfully updated car!!!');
+   setShow(false);
+  }
 
 
-  const [show,setShow] = useState(false);
+
 
   const closeModal = () => {
     setShow(false);
   }
-
-
 
     return (
         <div className="mt-5">
@@ -53,7 +76,7 @@ function Table({ products, onClick,deleteCar }) {
       <td>{car.price}$</td>
       <td>{car.markName}</td>
       <td><img src={car.image} style={{width:'5rem'}} alt="{car.model}"/></td>
-      <td><button className="btn btn-outline-light" id={car.id} onClick={()=>{setShow(true)}}>Edit</button></td>
+      <td><button className="btn btn-outline-light" id={car.id} onClick={handleEditCar}>Edit</button></td>
       <td><button className="btn btn-outline-danger" id={car.id} onClick={(e)=>handleDeleteCar(e)}>Delete</button></td>
     </tr>
   )}
@@ -62,15 +85,18 @@ function Table({ products, onClick,deleteCar }) {
 <Modal show={show}>
       <Modal.Header>Edit car</Modal.Header>
       <Modal.Body className="d-flex flex-column">
-          <input type="text"   className="input-form" placeholder="name"/>
-          <input type="text"     placeholder="image"/>
-          <input type="text"   placeholder="price"/>
-          <select >
+          <input type="text" value={name}   className="input-form" placeholder="name"/>
+          <input type="text" value={image}    placeholder="image"/>
+          <input type="text" value={price}   placeholder="price"/>
+          <select value={model} >
                <option value="">Select mark</option>
+               {mark.map(mark=>
+                  <option value={mark.id}>{mark.name}</option>
+              )}
           </select>
       </Modal.Body>
       <Modal.Footer className="d-flex justify-content-between">
-          <button className="btn btn-secondary">Edit</button>
+          <button className="btn btn-secondary" onClick={submitEditCar}>Submit</button>
           <button className="btn btn-danger" onClick={closeModal}>Close</button>
           </Modal.Footer>
     </Modal>
